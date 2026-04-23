da6edc891a04b5ab5b5d5afde2a8731e8b8dcc4b
ajoynul832-source <ajoynul832@gmail.com>
2026-04-23 14:09:26 -0400

    Update next.config.js

--- animex/frontend/next.config.js
+++ animex/frontend/next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${apiUrl}/:path*`
+      }
+    ];
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'cdn.noitatnemucod.net' },
